86b809ab7cc2152b37d96904692f7bcb6c1ae0c8
axdx-luke <tanluc.dev@gmail.com>
2026-06-23 08:46:30 +0700

    Add generated sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://luc.works',
   devToolbar: {
     enabled: false
   },
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
